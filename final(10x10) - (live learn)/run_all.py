e7ed1b1719632ceaf1ada1529aa11a112204965e
Dimitris Lillis <jimlillisjunior@gmail.com>
2025-06-06 16:29:09 +0300

    [remove emojis]

--- final(10x10) - (live learn)/run_all.py
+++ final(10x10) - (live learn)/run_all.py
@@ -2,13 +2,13 @@ import subprocess
 import time
 import os
 
-print("✅ Εκκίνηση εκπαίδευσης (main.py)...")
+print("Εκκίνηση εκπαίδευσης (main.py)...")
 subprocess.run(["python", "main.py"])
 
-print("✅ Εκπαίδευση ολοκληρώθηκε. Πίνακες αποθηκεύτηκαν.")
+print("Εκπαίδευση ολοκληρώθηκε. Πίνακες αποθηκεύτηκαν.")
 
-print("🎮 Είσαι έτοιμος για προσομοίωση.")
-print("➡ Πάτησε ENTER για να ξεκινήσει το pygame_simulation.py")
+print("Είσαι έτοιμος για προσομοίωση.")
+print("Πάτησε ENTER για να ξεκινήσει το pygame_simulation.py")
 input()
 
 # Τρέχει το pygame_simulation ξεχωριστά σε νέο παράθυρο
